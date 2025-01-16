7735fc0b1927244041c53c15ffd6d252ec8b4f01
Aleksandar Vujic <Aleksandar.Vujic@endava.com>
2025-01-16 10:20:31 +0100

    add about page with its style

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { SCP_LOGO } from '../../constants';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  scp_logo: string = SCP_LOGO;
 }
