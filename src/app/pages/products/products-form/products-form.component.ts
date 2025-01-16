66dbdb6e9fc1570e13b9d5354a4f01c936dfc4a0
Aleksandar Vujic <Aleksandar.Vujic@endava.com>
2025-01-16 15:40:20 +0100

    implementing shell for data and edit data

--- src/app/pages/products/products-form/products-form.component.ts
+++ src/app/pages/products/products-form/products-form.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products-form',
   standalone: true,
   imports: [],
   templateUrl: './products-form.component.html',
-  styleUrl: './products-form.component.scss'
+  styleUrl: './products-form.component.scss',
 })
-export class ProductsFormComponent {
+export class ProductsFormComponent implements OnInit {
+  editmode: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onCancel() {
+    this.router.navigate(['products']);
+  }
 }
--- src/app/pages/products/products-list/products-list.component.ts
+++ src/app/pages/products/products-list/products-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../../../models/product';
+import { ProductsService } from '../../../services/products-service/products.service';
+import { Router, RouterLink } from '@angular/router';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, CurrencyPipe, RouterLink],
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.scss'
+  styleUrl: './products-list.component.scss',
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+  products: IProduct[] = [];
 
+  constructor(
+    private productService: ProductsService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this._getProducts();
+  }
+
+  private _getProducts() {}
+
+  deleteProduct(productId: string) {}
+
+  updateProductRedirect(productId?: string) {
+    this.router.navigateByUrl(`product/form/${productId}`);
+  }
 }
